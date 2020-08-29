3c234cad6674621354c9c08f178a54f088b6a72d
Myke <16993755+MykeeBee@users.noreply.github.com>
2020-08-29 16:50:43 +0100

    Add project files.

---
+++ LetterOrder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace LetterOrder
+{
+    class Program
+    {
+        static void Main()
+        {
+            var file = File.ReadAllLines(@"C:\Users\Myke\source\repos\LetterOrder\words_alpha.txt");
+            var list = new List<string>(file);
+            
+            var wordList = new List<string>();
+            var addWord = false;
+
+            foreach (var word in list)
+            {
+                for (var i = 0; i < word.Length - 1; i++)
+                {
+                    if (word[i] <= word[i + 1] && word.Length >= 5)
+                    {
+                        addWord = true;
+                    }
+
+                    else
+                    {
+                        addWord = false;
+                        break;
+                    }
+                }
+                
+                if (addWord)
+                {
+                    wordList.Add(word);
+                }
+            }
+
+            foreach (var word in wordList)
+            {
+                Console.WriteLine(word + " - " + word.Length);
+            }
+        }
+    }
+}
